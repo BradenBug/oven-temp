8c46eae4aae49ed550626590e76ce23a527dbbcd
bradenbug <bradenwicker@gmail.com>
2019-11-27 00:31:43 -0800

    set up bootstrap, sketched out basic layout, started toggle between celcius and fahrenheit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,81 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import Jumbotron from "react-bootstrap/Jumbotron";
+import Container from "react-bootstrap/Container";
+import ToggleButtonGroup from "react-bootstrap/ToggleButtonGroup";
+import ToggleButton from "react-bootstrap/ToggleButton";
+import Row from "react-bootstrap/Row";
+
+import "./index.css";
+
+var unit = "F";
+
+class UnitSelect extends React.Component {
+  handleChange(val) {
+    unit = val;
+    console.log(val);
+  }
+
+  render() {
+    return (
+      <ToggleButtonGroup type="radio" name="unit" onChange={this.handleChange} defaultValue={"F"}>
+        <ToggleButton name="unit" value={"F"}>Fahrenheit</ToggleButton>
+        <ToggleButton name="unit" value={"C"}>Celsius</ToggleButton>
+      </ToggleButtonGroup>
+    );
+  }
+}
+
+class Temp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      fahrenheit: 32,
+      celsius: 0,
+    };
+  }
+
+  render() {
+    var currentTemp;
+
+    if (unit === "F") {
+      currentTemp = this.state.fahrenheit
+    } else {
+      currentTemp = this.state.celsius
+    }
+
+    return (
+      <h1 className="display-4 text-center text-dark">{currentTemp}° {unit}</h1>
+    );
+  }
+}
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Container>
+          <h1 className="display-3 text-center text-dark font-weight-bold">Braden's Oven Temperature</h1>
+        </Container>
+        <Container>
+            <Row className="justify-content-md-center">
+              <Temp />
+            </Row>
+            <Row className="justify-content-md-center">
+              <UnitSelect />
+            </Row>
+        </Container>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+    <App />,
+    document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
